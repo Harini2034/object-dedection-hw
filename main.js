06744e2ff9a7ad91f6b2e521d4d8fb2fef6fd6fd
Alexandria <72262425+Harini2034@users.noreply.github.com>
2021-06-08 19:52:43 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,57 @@ function dog()
 {
     window.location = ("dog.html");
 }
+
+img = "";
+status = "";
+objects = [];
+
+function preload()
+{
+    img = loadImage('dog_cat.jpg')
+}
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDedector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded !");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+
+    if(status != "")
+    {
+        for (i = 0;  i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status: Object Dedected";
+
+            fill("#FF0000");
+            percent = floor(object[i].y, object[i].width, object[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
